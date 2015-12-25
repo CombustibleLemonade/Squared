29957f0c425e7c6d1d6fcf8a7610f6b0de813745
CombustibleLemonade <heemstrajan@gmail.com>
2015-12-25 16:57:57 +0100

    added credits menu back in

--- Scripts/Menus/credits_menu.gd
+++ Scripts/Menus/credits_menu.gd
@@ -1,5 +1,11 @@
 extends "submenu.gd"
 
 func pressed(e):
+	if e.get_name() == "store_page":
+		OS.shell_open("http://combustible-lemonade.itch.io/squared")
+	if e.get_name() == "artist":
+		OS.shell_open("https://soundcloud.com/unfa")
+	if e.get_name() == "font":
+		OS.shell_open("http://www.dafont.com/accuratist.font")
 	if e.get_name() == "back":
 		top_menu.pop_active_menu()
